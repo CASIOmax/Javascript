b66ef0f3f8ce6ba6f82035ed95905fc4d943354a
Sajjad Ahmad <112914285+CASIOmax@users.noreply.github.com>
2024-08-03 07:45:24 +0000

    completed lec 04

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID=23                     /* Const = reamains contant */
+let accountEmail="231@gmail.com"
+var accountpass="1234"
+accountcity="Jaipur"                  //dont use this too dangerous
+
+// accountID=2   not allowed
+console.log(accountID);
+/*
+Dont use var it's old, has issue in block and functional scope 
+*/
+
+accountEmail="231@gmail.gfhtry"
+accountpass="3242"
+accountcity="Jaiperw42ur"
+
+console.table([accountEmail,accountID,accountpass, accountcity])
