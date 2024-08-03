44b48832fd27f1ccb652bbbf4a7fa0286fe2e0d2
Sajjad Ahmad <112914285+CASIOmax@users.noreply.github.com>
2024-08-03 12:45:39 +0000

    Dattypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat codeas as nwe version javascript
+
+// alert(3+3)    // we are using node js not browser
+console.log(3+3) 
+console.log("sajjad")
+
+let name="sajjad"
+let age=15
+let isLoggedOn=false
+let state = null          //like you have a server you ask for it's temperature but due to a reaso it was unable to send you the temperature so it sends 0 degree centigrade but 0 is not the temperature so we want nul
+
+// null => Standalone value
+//undefined=> like let state;
+//symbol => To find uniquessnes, for unique
+
+//object
+console.log(typeof null);      //is an object
+console.log(typeof undefined);   // is undefined
