416fd9f3e9045bf37bb8808025f46d8499e281fb
Sajjad Ahmad <112914285+CASIOmax@users.noreply.github.com>
2024-08-27 17:25:17 +0000

    peace out

---
+++ 10_classes_and_oop/Prototype.js
@@ -0,0 +1,18 @@
+// let myName="Sajjad"
+
+// console.log(myName.truelength);
+
+let myHeroe = ["thor","spide","bat"]
+
+let heroPower={
+    thor:"hammer",
+    spide:"sling",
+    bat : "money",
+
+    getSpiderPower:function(){
+        console.log(`Spide power is ${this.spide}`);
+    }
+}
+
+
+heroPower.sajjad()
